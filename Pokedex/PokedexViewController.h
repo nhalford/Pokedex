7bbed2bc257a37da8fce60d6a500e00ec88ac958
nhalford <noah.halford@gmail.com>
2014-03-12 21:58:34 -0500

    Set up the table view

---
+++ Pokedex/PokedexViewController.h
@@ -0,0 +1,13 @@
+//
+//  PokedexViewController.h
+//  Pokedex
+//
+//  Created by Noah Halford on 3/12/14.
+//  Copyright (c) 2014 Noah Halford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokedexViewController : UITableViewController
+
+@end
