bdb13a24f6f296668f470de8aa0d30a152328805
nhalford <noah.halford@gmail.com>
2014-03-12 20:05:16 -0500

    Beginning of the project. All data files included.

---
+++ Pokedex/NHPokemon.h
@@ -0,0 +1,13 @@
+//
+//  NHPokemon.h
+//  Pokedex
+//
+//  Created by Noah Halford on 3/12/14.
+//  Copyright (c) 2014 Noah Halford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NHPokemon : NSObject
+
+@end
---
+++ Pokedex/NHPokemon.m
@@ -0,0 +1,13 @@
+//
+//  NHPokemon.m
+//  Pokedex
+//
+//  Created by Noah Halford on 3/12/14.
+//  Copyright (c) 2014 Noah Halford. All rights reserved.
+//
+
+#import "NHPokemon.h"
+
+@implementation NHPokemon
+
+@end
